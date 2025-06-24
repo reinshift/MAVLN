94eca421a2e8596dd4a89497194c68fd39161256
reinshift <594004237@qq.com>
2025-06-24 11:13:03 +0800

    added bridge script for airsim simulator

---
+++ airsim_bridge.py
@@ -0,0 +1,16 @@
+"""
+when env_airsim simulator is started, this bridge script will catch actions that UAVs have taken, 
+and control UAVs to take the corresponding actions in simulator.
+"""
+class AirSimBridge:
+    def __init__(self):
+        pass
+
+    def get_uav_id(self):
+        pass
+
+    def set_uav_position(self, uav_id, position):
+        pass
+
+    def catch_action(self, action):
+        pass
